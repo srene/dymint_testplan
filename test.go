cf38b7a7a403f3d2636a588272cebc035c897283
Sergi Rene <sergi@datahop.network>
2023-11-28 15:09:34 +0100

    dymint nodes starting

--- test.go
+++ test.go
@@ -3,63 +3,127 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
 	tcfg "github.com/tendermint/tendermint/config"
-
-	"github.com/tendermint/tendermint/crypto"
-	tmnode "github.com/tendermint/tendermint/node"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"github.com/tendermint/tendermint/libs/log"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
 	tmp2p "github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/proxy"
-
-	tgsync "github.com/testground/sdk-go/sync"
+	"github.com/tendermint/tendermint/types"
+	tmtime "github.com/tendermint/tendermint/types/time"
 
 	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
+	tgsync "github.com/testground/sdk-go/sync"
 
 	"github.com/dymensionxyz/dymint/config"
 	"github.com/dymensionxyz/dymint/conv"
 	"github.com/dymensionxyz/dymint/node"
 )
 
-type NodeKey struct {
-	PrivKey crypto.PrivKey `json:"priv_key"` // our priv key
-}
+// setupNetwork instructs the sidecar (if enabled) to setup the network for this
+// test case.
+func setupNetwork(ctx context.Context, runenv *runtime.RunEnv, netclient *network.Client, latencyMin int, latencyMax int, bandwidth int) (*network.Config, error) {
+	if !runenv.TestSidecar {
+		return nil, nil
+	}
 
-func startDymintNode(config *config.NodeConfig, tmConfig *tcfg.Config) (*node.Node, error) {
-	nodeKey, err := tmp2p.LoadOrGenNodeKey(tmConfig.NodeKeyFile())
+	// Wait for the network to be initialized.
+	runenv.RecordMessage("Waiting for network initialization")
+	err := netclient.WaitNetworkInitialized(ctx)
 	if err != nil {
 		return nil, err
 	}
-	privValKey, err := tmp2p.LoadOrGenNodeKey(tmConfig.PrivValidatorKeyFile())
+	runenv.RecordMessage("Network init complete")
 
-	genDocProvider := tmnode.DefaultGenesisDocProviderFunc(tmConfig)
+	lat := rand.Intn(latencyMax-latencyMin) + latencyMin
+
+	bw := uint64(bandwidth) * 1000 * 1000
+
+	runenv.RecordMessage("Network params %d %d", lat, bw)
+
+	config := &network.Config{
+		Network: "default",
+		Enable:  true,
+		Default: network.LinkShape{
+			Latency:   time.Duration(lat) * time.Millisecond,
+			Bandwidth: bw, //Equivalent to 100Mps
+		},
+		CallbackState: "network-configured",
+		RoutingPolicy: network.DenyAll,
+	}
+
+	// random delay to avoid overloading weave (we hope)
+	delay := time.Duration(rand.Intn(1000)) * time.Millisecond
+	<-time.After(delay)
+	err = netclient.ConfigureNetwork(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func startDymintNode(runenv *runtime.RunEnv, config *config.NodeConfig, tmConfig *tcfg.Config) (*node.Node, error) {
+
+	/*nodeKey, err := tmp2p.LoadOrGenNodeKey(tmConfig.NodeKeyFile())
+
+	if err != nil {
+		return nil, err
+	}*/
+	privKey := ed25519.GenPrivKey()
+	nodeKey := &tmp2p.NodeKey{
+		PrivKey: privKey,
+	}
+
+	runenv.RecordMessage("nodekey loaded")
+
+	runenv.RecordMessage("privvalkey loaded")
+
+	//genDocProvider := tmnode.DefaultGenesisDocProviderFunc(tmConfig)
 	p2pKey, err := conv.GetNodeKey(nodeKey)
 	if err != nil {
 		return nil, err
 	}
-	signingKey, err := conv.GetNodeKey(privValKey)
+
+	//genesis, err := genDocProvider()
+	//runenv.RecordMessage("Genesis file %s", tmConfig.GenesisFile())
+	/*genesis, err := types.GenesisDocFromFile(tmConfig.GenesisFile())
 	if err != nil {
 		return nil, err
+	}*/
+
+	genDoc := types.GenesisDoc{
+		ChainID:         fmt.Sprintf("test-chain-%v", tmrand.Str(6)),
+		GenesisTime:     tmtime.Now(),
+		ConsensusParams: types.DefaultConsensusParams(),
 	}
-	genesis, err := genDocProvider()
-	if err != nil {
-		return nil, err
-	}
+	pubKey := privKey.PubKey()
+	genDoc.Validators = []types.GenesisValidator{{
+		Address: pubKey.Address(),
+		PubKey:  pubKey,
+		Power:   10,
+	}}
 	err = conv.GetNodeConfig(config, tmConfig)
 	if err != nil {
 		return nil, err
 	}
 	//logger.Info("starting node with ABCI dymint in-process", "conf", config)
 
+	tmConfig.ProxyApp = "kvstore"
 	dymintNode, err := node.NewNode(
 		context.Background(),
 		*config,
 		p2pKey,
-		signingKey,
+		p2pKey,
 		proxy.DefaultClientCreator(tmConfig.ProxyApp, tmConfig.ABCI, tmConfig.DBDir()),
-		genesis,
-		nil,
+		&genDoc,
+		log.NewTMLogger(log.NewSyncWriter(os.Stdout)),
 	)
 	if err != nil {
 		return nil, err
@@ -103,11 +167,11 @@ func test(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 	tmconfig := tcfg.DefaultConfig()
 	dymconfig := config.DefaultNodeConfig
 
-	node, err := startDymintNode(&dymconfig, tmconfig)
+	node, err := startDymintNode(runenv, &dymconfig, tmconfig)
 	if err != nil {
 		return err
 	}
-	runenv.RecordMessage("Dymint node started %s", node.P2P.Addrs())
+	runenv.RecordMessage("Dymint node started %s", node)
 	//peers := tgsync.NewTopic("nodes", &peer.AddrInfo{})
 
 	// Get sequence number within a node type (eg honest-1, honest-2, etc)
@@ -120,7 +184,7 @@ func test(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 
 	runenv.RecordMessage("before netclient.MustConfigureNetwork")
 
-	//config, err := setupNetwork(ctx, runenv, netclient, params.netParams.latency, params.netParams.latencyMax, params.netParams.bandwidthMB)
+	_, err = setupNetwork(ctx, runenv, netclient, params.netParams.latency, params.netParams.latencyMax, params.netParams.bandwidthMB)
 	if err != nil {
 		return fmt.Errorf("Failed to set up network: %w", err)
 	}
